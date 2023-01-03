94a2d0e5bfd5aeec50c346a5c27c0151662acf82
Joe Huss <detain@interserver.net>
2023-01-03 15:20:10 -0500

    fixing some problems in update-samples where it was not getting the right swagger info

--- update-samples.php
+++ update-samples.php
@@ -53,8 +53,8 @@ if ($buildSwagger === true) {
 	echo "Determining latest Swagger Generator jar\n";
 	$prefix = 'https://oss.sonatype.org/content/repositories/snapshots/io/swagger/codegen/v3/swagger-codegen-cli/';
 	$branch = trim(`curl -s {$prefix}|grep SNAPSHOT|sort|tail -n 1|cut -d/ -f12|cut -d- -f1`);
-	$latest = trim(`curl -s {$prefix}/{$branch}-SNAPSHOT/|grep "[0-9].jar<"|cut -d\" -f2|sort|tail -n 1`);
-	echo "Grabbing latest Swagger Generator jar {$latest}\n";
+	$latest = trim(`curl -s {$prefix}{$branch}-SNAPSHOT/|grep "[0-9].jar<"|cut -d\" -f2|sort|tail -n 1`);
+	echo "Grabbing {$branch} latest {$latest} Swagger Generator jar {$latest}\n";
 	passthru('cd '.__DIR__.' && wget -q "'.$latest.'" -O swagger-codegen-cli.jar');
 	echo "Generating and parsing a list of Swagger Generator clients we can generate\n";
 	$langs = explode(', ', trim(exec('cd '.__DIR__.' && java -jar swagger-codegen-cli.jar langs | cut -d \[ -f2-|cut -d\] -f1')));
