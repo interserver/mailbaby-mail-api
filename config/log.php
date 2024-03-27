83bc47bf52d0c5723981f987486f43be468276a2
Joe Huss <detain@interserver.net>
2024-03-26 20:20:12 -0400

    logging update

--- config/log.php
+++ config/log.php
@@ -16,12 +16,17 @@ return [
     'default' => [
         'handlers' => [
             [
-                'class' => Monolog\Handler\RotatingFileHandler::class,
+                'class' => Monolog\Handler\StreamHandler::class,
+                'constructor' => [
+                    runtime_path() . '/logs/webman.log',
+                    Monolog\Logger::DEBUG,
+                ],
+                /* 'class' => Monolog\Handler\RotatingFileHandler::class,
                 'constructor' => [
                     runtime_path() . '/logs/webman.log',
                     7, //$maxFiles
                     Monolog\Logger::DEBUG,
-                ],
+                ], */
                 'formatter' => [
                     'class' => Monolog\Formatter\LineFormatter::class,
                     'constructor' => [null, 'Y-m-d H:i:s', true],
